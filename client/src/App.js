f3a4faa42334a661d2d6763dc95f09daa887ea9c
peter9012 <rambo_lu@hotmail.com>
2023-04-08 22:45:10 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import './normal.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
