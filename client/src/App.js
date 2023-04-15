9cc4763e5f0bde95815c3da4cf195b375283b4f0
peter9012 <rambo_lu@hotmail.com>
2023-04-15 14:32:00 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,15 @@ function App() {
     <div className="App">
       <aside className="sidemenu">
         <div className="side-menu-button">
+          <span>+</span>
           New Chat
         </div>
       </aside>
       <section className="chatbox">
-
+        <div className="chat-input-holder">
+          <textarea className="chat-input-textarea" placeholder="Type your message here">
+          </textarea>
+        </div>
       </section>
     </div>
   );
