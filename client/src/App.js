8feacd83fe5ad324b9d445afbe576e8aec355a06
peter9012 <rambo_lu@hotmail.com>
2023-06-06 22:27:55 -0700

    Styling the Chat

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,15 @@ function App() {
               </div>
             </div>
           </div>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar">
+              </div>
+              <div className="message">
+                I am an AI
+              </div>
+            </div>
+          </div>
         </div>
         <div className="chat-input-holder">
           <textarea rows="1" className="chat-input-textarea">
